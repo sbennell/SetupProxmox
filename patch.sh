e8a3f8432c4508a3a245423a24f4ce95b9d5f076
sbennell <stewart@bennell.me>
2019-11-13 08:26:14 +1100

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -3,7 +3,7 @@
 mkdir -p /usr/share/pve-patch/{images,scripts}
 echo "- patch `pveversion`..."
 echo "- download and copy files..."
-wget -nc -qP /usr/share/pve-patch/images/  https://raw.githubusercontent.com/sbennell/pve-patch/raw/master/images/{favicon.ico,logo-128.png,proxmox_logo.png}
+wget -nc -qP /usr/share/pve-patch/images/  https://github.com/sbennell/pve-patch/tree/master/images/{favicon.ico,logo-128.png,proxmox_logo.png}
 rm -f /usr/share/pve-patch/scripts/{90pvepatch,apply.sh}
 wget -qP /usr/share/pve-patch/scripts/  https://raw.githubusercontent.com/sbennell/pve-patch/master/scripts/{90pvepatch,apply.sh}
 chmod -R a+x /usr/share/pve-patch/scripts
