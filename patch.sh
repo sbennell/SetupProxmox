9dc385f12e6346dd43d9875c0e61ef762f4b5140
Stewart Bennell <stewart@bennell.me>
2020-05-28 13:39:42 +1000

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -29,7 +29,7 @@ chmod -R go= ~/.ssh
 echo "- Seting up smtp for email alerts"
 apt install postfix sasl2-bin mailutils -y
 apt install libsasl2-modules -y
-#wget -qP /etc/postfix/ https://raw.githubusercontent.com/sbennell/pve-patch/master/mail/{mailtest.txt,main.cf,sasl_passwd.db,sender_canonical.db}
+wget --user=bennellit --ask-password -qP /etc/postfix/ https://bennellit.com.au/Files/noaccess/{mailtest.txt,main.cf,sasl_passwd.db,sender_canonical.db}
 service postfix restart
 sendmail -v server@lab-network.xyz
 
