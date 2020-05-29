736f817cea2b53d38e75eeb242e9ed67612f96a8
Stewart Bennell <stewart@bennell.me>
2020-05-29 19:27:13 +1000

    Update 29/05/20

--- patch.sh
+++ patch.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-echo "- Apt Update and upgrade system..."
-apt update
-apt upgrade -y
-echo "- Install Packages."
-apt install ifupdown2 postfix sasl2-bin mailutils libsasl2-modules -y 
-
 mkdir -p /usr/share/pve-patch/{images,scripts}
 echo "- patch `pveversion`..."
 echo "- download and copy files..."
@@ -20,18 +14,33 @@ cp -f /usr/share/pve-patch/scripts/90pvepatch /etc/apt/apt.conf.d/90pvepatch
 cp -f /usr/share/pve-patch/scripts/pvebanner /usr/bin/pvebanner
 /usr/share/pve-patch/scripts/apply.sh
 
+echo "- Apt Update and upgrade system..."
+apt update
+apt update && apt dist-upgrade -y
+echo "- Install Packages."
+apt install ifupdown2 sasl2-bin mailutils libsasl2-modules -y curl
+
 echo "- Adding SSH Key - Bennell IT..."
 mkdir -p ~/.ssh 
 touch ~/.ssh/authorized_keys
 echo ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAkXk0+tC1ZMiWgTQvE/GeB9+TuPWTf8mr9zVOYdNhF+KFXxc/DjMjIPNCAUxtQErlush1GF87b7gaEIC2F5p/+xr39gnt5panjT2AJmVQm9GrSc0LwZOHducgB9SeW7F6A2hA0dtEDxOPHC88ipT9qvTZdeC+mgoNmyIAIMmnPVcZOqQm7iVUf3kJCRWVGI/csE1UYpZ1tLpkaNqjP0Iy7cQvNgodJWh8Mg//TD6ESKBQ35P3+6zT2zEpIK/hQ5eaW5Uu82kSt1ZGuNaPukfCra0cjWr2n4hC+C3E9m3K/3ZV43usaxwSbPa6R/jJE4fyqpC2hqdTKW8Z66mVTC8EpQ== Bennell IT >> ~/.ssh/authorized_keys
 chmod -R go= ~/.ssh
 
-#echo "- Seting up smtp for email alerts"
+echo "- Setting  up smtp for email alerts"
+#remove file if exists
+rm -f /etc/postfix/{main.cf,mailtest.txt,sasl_passwd,sender_canonical}
+#Downloading Files
+wget -nc -qP /etc/postfix/ https://raw.githubusercontent.com/sbennell/pve-patch/master/mail/main.cf
 
-rm -f /etc/postfix/{mailtest.txt,sasl_passwd,sender_canonical}
-wget -nc -P /etc/postfix/ https://raw.githubusercontent.com/sbennell/pve-patch/master/mail/mailtest.txt
-#wget -nc -P /etc/postfix/ https://raw.githubusercontent.com/sbennell/pve-patch/master/mail/main.cf
-wget --user=bennellit --ask-password -P /etc/postfix/ https://bennellit.com.au/Files/noaccess/{sasl_passwd,sender_canonical}
+echo Enter Office 365 Email Address?
+read Email
+
+echo Enter Office 365 Email Password?
+read Password
+
+echo "[smtp.office365.com]:587 $Email:$Password" >> /etc/postfix/sasl_passwd
+echo "/.+/ $Email" >> /etc/postfix/sender_canonical
+
 postmap hash:/etc/postfix/sasl_passwd
 postmap hash:/etc/postfix/sender_canonical
 cp /etc/ssl/certs/thawte_Primary_Root_CA.pem /etc/postfix/cacert.pem
@@ -40,6 +49,15 @@ chmod 644 /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
 chown root:root /etc/postfix/sender_canonical /etc/postfix/sender_canonical.db  
 chmod 644 /etc/postfix/sender_canonical /etc/postfix/sender_canonical.db
 service postfix restart
+
+Serverfqdn=$(hostname -f)
+IP=$(hostname -I)
+
+echo "to: server@bennellit.com.au" >> /etc/postfix/emailsetupinfo.txt
+echo "subject:New Server Setup Info $Serverfqdn" >> /etc/postfix/emailsetupinfo.txt
+echo "Hostname: $Serverfqdn" >> /etc/postfix/emailsetupinfo.txt
+echo "IP Address: $IP" >> /etc/postfix/emailsetupinfo.txt
+
 sendmail -v server@lab-network.xyz < /etc/postfix/mailtest.txt
 
 echo "- done!"
