3771fe7cfc340ce640b23d33062f69a679d412fd
Stewart Bennell <stewart@bennell.me>
2021-09-24 08:41:39 +1000

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -4,11 +4,11 @@ mkdir -p /usr/share/pve-patch/{images,scripts}
 echo "- patch `pveversion`..."
 echo "- download and copy files..."
 rm -f /usr/share/pve-patch/images/{favicon.ico,logo-128.png,proxmox_logo.png}
-wget -nc -qP /usr/share/pve-patch/images/ https://raw.githubusercontent.com/sbennell/pve-patch/master/images/favicon.ico
-wget -nc -qP /usr/share/pve-patch/images/ https://raw.githubusercontent.com/sbennell/pve-patch/master/images/logo-128.png
-wget -nc -qP /usr/share/pve-patch/images/ https://raw.githubusercontent.com/sbennell/pve-patch/master/images/proxmox_logo.png
+wget -nc -qP /usr/share/pve-patch/images/ https://raw.githubusercontent.com/sbennell/pve-patch/Testing/images/favicon.ico
+wget -nc -qP /usr/share/pve-patch/images/ https://raw.githubusercontent.com/sbennell/pve-patch/Testing/images/logo-128.png
+wget -nc -qP /usr/share/pve-patch/images/ https://raw.githubusercontent.com/sbennell/pve-patch/Testing/images/proxmox_logo.png
 rm -f /usr/share/pve-patch/scripts/{90pvepatch,apply.sh,pvebanner}
-wget -qP /usr/share/pve-patch/scripts/ https://raw.githubusercontent.com/sbennell/pve-patch/master/scripts/{90pvepatch,apply.sh,pvebanner}
+wget -qP /usr/share/pve-patch/scripts/ https://raw.githubusercontent.com/sbennell/pve-patch/Testing/scripts/{90pvepatch,apply.sh,pvebanner}
 chmod -R a+x /usr/share/pve-patch/scripts
 cp -f /usr/share/pve-patch/scripts/90pvepatch /etc/apt/apt.conf.d/90pvepatch
 rm -f /usr/bin/pvebanner
