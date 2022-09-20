0ebcc58a413886328d6a856f51e54a7a98389e74
Stewart Bennell <stewart@bennell.me>
2022-09-20 10:24:49 +1000

    Update subscription.sh

--- scripts/subscription.sh
+++ scripts/subscription.sh
@@ -7,4 +7,3 @@ curl -s https://api.github.com/repos/sbennell/pve-fake-subscription/releases/lat
 dpkg -i pve-fake-subscription_*.deb
 rm -f pve-fake-subscription_*.deb
 echo "127.0.0.1 shop.maurer-it.com" | tee -a /etc/hosts
-wget -qP /etc/apt/apt.conf.d/ https://raw.githubusercontent.com/sbennell/pve-patch/$branches/scripts/70BITsubscription
