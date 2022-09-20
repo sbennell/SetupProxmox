f9d51f391a094efb10418c40fa1d4f334d2df479
Stewart Bennell <stewart@bennell.me>
2022-09-20 12:38:05 +1000

    Update darkmode.sh

--- scripts/darkmode.sh
+++ scripts/darkmode.sh
@@ -1,1 +1,1 @@
-bash <(curl -s https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh ) install  &
+bash <(curl -s https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh ) install
