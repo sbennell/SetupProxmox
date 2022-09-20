0bd58180c2233943f7fce9c26e4ff79598dd10f3
Stewart Bennell <stewart@bennell.me>
2022-09-20 14:12:32 +1000

    Update subscription.sh

--- scripts/subscription.sh
+++ scripts/subscription.sh
@@ -4,6 +4,6 @@ curl -s https://api.github.com/repos/sbennell/pve-bit-subscription/releases/late
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi - 
-dpkg -i pve-fake-subscription_*.deb &>/dev/null
-rm -f pve-fake-subscription_*.deb &>/dev/null
+dpkg -i pve-bit-subscription_*.deb &>/dev/null
+rm -f pve-bit-subscription_*.deb &>/dev/null
 echo "127.0.0.1 shop.maurer-it.com" | tee -a /etc/hosts &>/dev/null
