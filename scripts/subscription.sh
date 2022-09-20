debb128d281078e78ef27f9e2549df4e0c17d23a
Stewart Bennell <stewart@bennell.me>
2022-09-20 14:24:14 +1000

    Update subscription.sh

--- scripts/subscription.sh
+++ scripts/subscription.sh
@@ -1,9 +1,9 @@
-apt purge pve-bit-subscription &>/dev/null
+apt purge pve-bit-subscription -y &>/dev/null
 curl -s https://api.github.com/repos/sbennell/pve-bit-subscription/releases/latest \
 | grep "browser_download_url.*deb" \
 | cut -d : -f 2,3 \
 | tr -d \" \
-| wget -qi - 
+| wget -qi -
 dpkg -i pve-bit-subscription_*.deb &>/dev/null
 rm -f pve-bit-subscription_*.deb &>/dev/null
 echo "127.0.0.1 shop.maurer-it.com" | tee -a /etc/hosts &>/dev/null
