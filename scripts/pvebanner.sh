2ac8b066348a9fe3891a0dfeb397c17b26dc3b82
Stewart Bennell <stewart@bennell.me>
2022-09-20 10:30:58 +1000

    Update pvebanner.sh

--- scripts/pvebanner.sh
+++ scripts/pvebanner.sh
@@ -1,3 +1,3 @@
-wget -qP /usr/bin/ https://raw.githubusercontent.com/sbennell/pve-patch/$branches/scripts/pvebanner
+wget -qP /usr/bin/ https://raw.githubusercontent.com/sbennell/pve-patch/master/scripts/pvebanner
 echo "- Updating logon banner..."
 /usr/bin/pvebanner
