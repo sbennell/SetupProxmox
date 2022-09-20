e0c77a9d84850ac0681854e70e82cd9e59801545
Stewart Bennell <stewart@bennell.me>
2022-09-20 10:38:47 +1000

    Update pvebanner.sh

--- scripts/pvebanner.sh
+++ scripts/pvebanner.sh
@@ -1,3 +1,29 @@
+#!/usr/bin/env bash -ex
+set -euo pipefail
+shopt -s inherit_errexit nullglob
+YW=`echo "\033[33m"`
+BL=`echo "\033[36m"`
+RD=`echo "\033[01;31m"`
+BGN=`echo "\033[4;92m"`
+GN=`echo "\033[1;92m"`
+DGN=`echo "\033[32m"`
+CL=`echo "\033[m"`
+BFR="\\r\\033[K"
+HOLD="-"
+CM="${GN}✓${CL}"
+CROSS="${RD}✗${CL}"
+clear
+function msg_info() {
+    local msg="$1"
+    echo -ne " ${HOLD} ${YW}${msg}..."
+}
+
+function msg_ok() {
+    local msg="$1"
+    echo -e "${BFR} ${CM} ${GN}${msg}${CL}"
+}
+clear
+
 wget -qP /usr/bin/ https://raw.githubusercontent.com/sbennell/pve-patch/master/scripts/pvebanner 
-echo "- Updating logon banner..."
 /usr/bin/pvebanner &
+msg_ok "Done updating logon banner..."
