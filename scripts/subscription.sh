256423ac13dc12444f45a91c6b98d3cc3f462c1a
Stewart Bennell <stewart@bennell.me>
2022-09-20 10:35:54 +1000

    Update subscription.sh

--- scripts/subscription.sh
+++ scripts/subscription.sh
@@ -1,9 +1,9 @@
-apt purge pve-fake-subscription
+apt purge pve-fake-subscription &>/dev/null
 curl -s https://api.github.com/repos/sbennell/pve-fake-subscription/releases/latest \
 | grep "browser_download_url.*deb" \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi - 
-dpkg -i pve-fake-subscription_*.deb
-rm -f pve-fake-subscription_*.deb
-echo "127.0.0.1 shop.maurer-it.com" | tee -a /etc/hosts
+dpkg -i pve-fake-subscription_*.deb &>/dev/null
+rm -f pve-fake-subscription_*.deb &>/dev/null
+echo "127.0.0.1 shop.maurer-it.com" | tee -a /etc/hosts &>/dev/null
