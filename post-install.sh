eeeb12218afe7c131eda70eef79df5ef8ddd6ad7
Stewart Bennell <stewart@bennell.me>
2022-09-28 06:15:26 +1000

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -118,25 +118,7 @@ case $CHOICE in
 
 	"2)")   
 	    msg_info "Adding Beta/Test Repository and set disabled"
-	    if [ -d "$pve_log_folder" ]; then
-	    	echo "- Server is a PVE host"
-	    	echo "- Checking Sources list"
-	    	if grep -Fq "deb http://download.proxmox.com/debian/pve" /etc/apt/sources.list; then
-	    		echo "-- Source looks alredy configured - Skipping"
-	    	else
-	    		echo "-- Adding new entry to sources.list"
-	    		sed -i "\$adeb http://download.proxmox.com/debian/pve $distribution pvetest" /etc/apt/sources.list
-	    	fi
-	    else
-		echo "- Server is a PBS host"
-	    	echo "- Checking Sources list"
-	    	if grep -Fq "deb http://download.proxmox.com/debian/pve" /etc/apt/sources.list; then
-	    		echo "-- Source looks alredy configured - Skipping"
-	    	else
-	    		echo "-- Adding new entry to sources.list"
-	    		sed -i "\$a#deb http://download.proxmox.com/debian/pbs $distribution pbstest" /etc/apt/sources.list
-	    	fi	
-	    msg_ok "Added Beta/Test Repository"
+
 	;;
 
 	"3)")   
