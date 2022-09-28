dec2ae1e9f24b9205fc9fa5d161dc678e30243bc
Stewart Bennell <stewart@bennell.me>
2022-09-29 06:43:40 +1000

    Delete subscription.sh

--- scripts/subscription.sh
+++
@@ -1,9 +0,0 @@
-apt purge pve-bit-subscription -y &>/dev/null
-curl -s https://api.github.com/repos/sbennell/pve-bit-subscription/releases/latest \
-| grep "browser_download_url.*deb" \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget -qi -
-dpkg -i pve-bit-subscription_*.deb &>/dev/null
-rm -f pve-bit-subscription_*.deb &>/dev/null
-echo "127.0.0.1 shop.maurer-it.com" | tee -a /etc/hosts &>/dev/null
